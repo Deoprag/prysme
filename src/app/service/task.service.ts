7ffc68bff7880d1e4d2d3fe03fffa147e35c482a
Pedro Rocha <pdroesofiarabelo@gmail.com>
2024-10-10 12:58:14 -0300

    Adicionado services de Task e Team

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TaskService {
+    private baseUrl = 'http://localhost:8080/api/v1/task';
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    findAll(): Observable<Task[]> {
+        return this.http.get<Task[]>(this.baseUrl);
+    }
+
+    findById(id: number): Observable<Task> {
+        return this.http.get<Task>(`${this.baseUrl}/${id}`);
+    }
+
+    create(task: Task): Observable<Task> {
+        return this.http.post<Task>(`${this.baseUrl}/create`, task);
+    }
+
+    update(task: Task): Observable<Task> {
+        return this.http.put<Task>(`${this.baseUrl}/save`, task);
+    }
+
+    delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    }
 }
--- src/app/service/team.service.ts
+++ src/app/service/team.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Team } from '../model/team';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TeamService {
+    private baseUrl = 'http://localhost:8080/api/v1/team';
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    findAll(): Observable<Team[]> {
+        return this.http.get<Team[]>(this.baseUrl);
+    }
+
+    findById(id: number): Observable<Team> {
+        return this.http.get<Team>(`${this.baseUrl}/${id}`);
+    }
+
+    create(team: Team): Observable<Team> {
+        return this.http.post<Team>(`${this.baseUrl}/create`, team);
+    }
+
+    update(team: Team): Observable<Team> {
+        return this.http.put<Team>(`${this.baseUrl}/save`, team);
+    }
+
+    delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    }
 }
